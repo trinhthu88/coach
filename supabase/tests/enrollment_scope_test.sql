6b07d32024e3f0e7700751b320eca6bb62fb61c5
trinhthu88 <trinhthu.mktg@gmail.com>
2026-09-10 03:18:20 +0700

    test: use schema-qualified enrollment index assertion

--- supabase/tests/enrollment_scope_test.sql
+++ supabase/tests/enrollment_scope_test.sql
@@ -4,6 +4,6 @@ select has_function('public', 'create_programme_enrollment', array['uuid','uuid'
 select has_function('public', 'record_goal_checkin', array['uuid','uuid','text','uuid','smallint','text']);
 select has_function('public', 'get_enrollment_progress', array['uuid','date']);
 select has_function('public', 'generate_enrollment_schedule', array['uuid']);
-select has_index('public', 'programme_enrollments', 'ux_programme_enrollments_one_ongoing');
+select has_index('public', 'programme_enrollments', 'ux_programme_enrollments_one_ongoing', 'ongoing enrollment uniqueness index exists');
 select * from finish();
 rollback;
